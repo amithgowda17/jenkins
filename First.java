5f3e8b3a4aed159e02a451e982bcf398d4b2c027
xworkzdev3@gmail.com <xworkzdev3@gmail.com>
2025-07-24 19:38:49 +0530

    first

---
+++ First.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+class First {
+
+    public static void main(String[] args) {
+        System.out.println("this is first java program");
+        System.out.println(new Date());
+    }
+}
