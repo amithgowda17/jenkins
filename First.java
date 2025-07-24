4af13a5c85ae1d238107338f6b5190a2ef8570e0
xworkzdev3@gmail.com <xworkzdev3@gmail.com>
2025-07-24 19:46:30 +0530

    first

--- First.java
+++ First.java
@@ -5,5 +5,6 @@ class First {
     public static void main(String[] args) {
         System.out.println("this is first java program");
         System.out.println(new Date());
+        System.out.println("this is new project");
     }
 }
